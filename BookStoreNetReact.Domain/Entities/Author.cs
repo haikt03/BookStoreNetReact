015b4ea43c7689266a1af49309586c3ef06e5dd6
haikt03 <tuanhai1652003@gmail.com>
2024-10-29 00:03:20 +0700

    Create Entities

---
+++ BookStoreNetReact.Domain/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStoreNetReact.Domain.Entities
+{
+    public class Author : BaseEntity
+    {
+        public required string FullName { get; set; }
+        public string? Biography { get; set; }
+        public string? Country { get; set; }
+        
+        public string? PublicId { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public List<Book>? Books { get; set; }
+    }
+}
---
+++ BookStoreNetReact.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreNetReact.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
